b3e3fb43f2b712eb26b3c6a42ec44bdc8e774b78
Ruitong Zhang <r544zhan@uwaterloo.ca>
2025-09-19 15:10:32 -0400

    Edited README.md

---
+++ output/detection.js
@@ -0,0 +1,20 @@
+[
+    {
+        "timestamp": "2025-09-19 13:30:01",
+        "ip": "192.168.1.45",
+        "event": "Multiple failed logins",
+        "mitre": {
+            "technique": "T1110",
+            "name": "Brute Force"
+        }
+    },
+    {
+        "timestamp": "2025-09-19 13:30:05",
+        "ip": "192.168.1.45",
+        "event": "Suspicious successful login after failures",
+        "mitre": {
+            "technique": "T1078",
+            "name": "Valid Accounts"
+        }
+    }
+]
